8d946c8fb8e21e7524ecd6e95c36b3abdf6b8806
sarthaki-patil <sarthaki.patil93@gmail.com>
2022-11-14 17:13:31 +0530

    Add Company Model

--- app/models.py
+++ app/models.py
@@ -14,3 +14,17 @@ class Employee(models.Model):
     def __str__(self):
         return f"{self.last_name} -- {self.last_name}"
 
+
+class Company(models.Model):
+    name = models.CharField(max_length = 255)
+    owner_name = models.CharField(max_length = 255)
+    address = models.CharField(max_length = 255)
+    email = models.EmailField()
+    num_of_emp = models.IntegerField()
+  
+
+    class Meta:
+        db_table = "company"
+
+    def __str__(self):
+        return f"{self.name} -- {self.owner_name}"
